99516267dcd1e98bfa3cab967c1b24ff2464e0c5
sr <sunranlb@qq.com>
2022-11-23 22:25:54 +0800

    update

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ var request = require('request');
 var fs = require('fs');
 const LZString = require("lz-string")
 
-let img_data = "JYWw5g9ALAjArAZgggbHKUIAYYDoBWADmADSiSyLJobYBMBxZ408Sq6mWCjp5rVDrSxRezCm2qdscMf0rsaXFHJYKpwgOyqJgpdgAcOgYulYAnMfVDMMLFck2IMPET5rH+mAzfiTG2x5feU9pGFFgjz0w2UjdU1oYFTj/JxhtFOsvI0zQxMtc6No6e0KEzDpXJhCiip9qqPKIOiCG+IDmiLbU/TpY7qzpOmSBvIqM0drmnMmmugLZjoRS4iA"
+// just change this line, from .html file bottom
+let img_data = "JYWw5g9ATAzAHDAnBJAWA7O1EAMMD6OOAjAHQBWADmADSiSwLJqbZ6E5QXV3jTxIUiDFlwEiMbrXr8mQkW3E5UU3gwHNhrMRwCsqmY0EtR7IgDYDfI5oU6i6K+rknFHOE9nGtppYk828tpmxDgBGkG+hMRkVNLWEa72xFxxal62weLEkmmGiT5uJCp5CS6Fyfqlzt52IZbVGZFFxI6NgUkhHu0Fddn+PeV9hFBhg7VZI7E8+UOTnKkzZRNRnLlLNZmrUCUbTZ3iUFV7HRVmUA0nvfNQbVdz2933K0VQA89bRTBjH832MNN4ps/mYYIsgfszuIYOsIadhnhdnDrqsYMdkQ8vpcMS9/nccZ9/k8CSDoe8SQdCKgfhSoVTAel4fNUODGSiiqhYWzMfZUEjubizKh0QLCULsaLSVT8ZLKcpibK6cpyYqEboaar5roGbNBeJdKzdWL9VyjVKcLp+Wa5boRdalboJfa1TLnVqFW7VroVZ6iuYNb77OYdctjYRzIbQ+bzKao3LzFa40rzHakwjzE60/NzK6s6tzB6836fUX7OgA6WzOgQ8C5ehI7WlehY42EVhwjyq6nW/N0Jme6tMB29YR0IWB0V0CWJ/Y4BWZ2Y4DXIQi4A2V/MEMOwzg4ImF+I4N2N6s4P2T0U4LmD4Q4OOL7Ppw+zIh58/xIhl0zVoh19+iogLbvoQwjbuaiDHv+9iIOeUEvtewE4Ig95wR+T6ofgoRvhhoRfuyOihH++HsKEQE4coYGUqEkHEQQoSwbRmE4Ah5EoYxoToexoSUXSMR4Z2dEpDxwwxGRXH7sBMQ0QJmHEAxMkxCxXFsQpxCcQpozCZMKT8SOKRERpYkaRJOFHFpUQpPJem3OZbgpCp1nqXp3y2QRAKuSRYIeXRMLeZhMAmexaJ+TkVlhjkSkKfAIVICF1Jxbp4UsnFRl6XycXSWlYVSsQGBxQ5SVOeF6ohdqpUGXpuipcVgUKbapXZVRuiRZVBU5d6IX+p1iU5RGnXVb1tV6SmnWNbxOadW1VHmEVOXliF1YLRV4XNgtQ0rZlK1jSJQ7jCtU28VOIVzsdPVUWux0Ded605Uex3bdpV7HQdIlwLNVGviFn5fctOWAV9N0fZtf0PRZiAteFyFfe9dKjNhjGjGdsOcH5oxXcjgPI8DlKjKDbijPiQA"
 let originArr = LZString.decompressFromBase64(img_data).split(',');
 
 function start(i) {
@@ -18,7 +19,7 @@ function start(i) {
             if (res.statusCode != 200) {
                 throw new Error(res.statusCode);
             } else {
-                console.log("done " + (i + 1));
+                console.log("done " + (i + 1) + "/" + originArr.length);
                 start(i + 1);
             }
         })
