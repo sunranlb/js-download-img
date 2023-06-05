6dc199d5e94e5f9c38a85e3b73b53ff3e9744608
sr <sunranlb@qq.com>
2023-06-05 21:45:02 +0800

    download img from https://www.dmhua8.com/manhua/dongwurenjian/1233079.html

--- main.js
+++ main.js
@@ -1,88 +1,67 @@
-var request = require('request');
-var fs = require('fs');
-var util = require('util');
+const request = require('request');
+const fs = require('fs');
+const util = require('util');
 
-let proxy_ip = '127.0.0.1';
-let proxy_port = '7890';
-let proxy = util.format('http://%s:%d', proxy_ip, proxy_port);
+const proxy_ip = '127.0.0.1';
+const proxy_port = '7890';
+const proxy = util.format('http://%s:%d', proxy_ip, proxy_port);
 
-const SCALE = 0;
-let si = 0; // start i     0   10  100
-let sum = 1000;   // num
+// https://www.dmhua8.com/manhua/dongwurenjian/1233079.html
+
 let cookie = "";
 let referer = "";
-let url1 = "";
-let url2 = "";
-function readHAR() {
-    const fileContents = fs.readFileSync('/Users/sr/Downloads/4.har', 'utf-8');
-    const d = JSON.parse(fileContents);
-    const entries = d.log.entries;
-    if (entries) {
-        try {
-            entries.forEach((entry) => {
-                if (entry.request.url.indexOf('us.archive.org') > 0) {
-                    let url = entry.request.url;
-                    let idx = url.indexOf('.jp2&');
-                    url1 = url.substring(0, idx - 4) + '000';
-                    url2 = url.substring(idx);
-                    let len2 = url2.length;
-                    url2 = url2.substring(0, len2 - 10) + SCALE + url2.substring(len2 - 9);
-                    entry.request.headers.forEach((h) => {
-                        if (h.name == 'cookie') {
-                            cookie = h.value;
-                        } else if (h.name == 'referer') {
-                            referer = h.value;
-                        }
-                    });
-                    throw "sss";
-                }
-            })
-        } catch (e) {
-            if (e != "sss") {
-                throw e;
-            } else {
-                return true;
-            }
-        }
+const FROM_FILE_INDEX = 152;
+
+function readHTML() {
+    const fileContents = fs.readFileSync('index.html', 'utf-8');
+    const match = fileContents.match(/var chapterImages = (\[.*?\]);/);
+    if (match) {
+        const chapterImages = JSON.parse(match[1].replace(/\\/g, ''));
+        downloadImages(chapterImages, 0);
+    } else {
+        console.error('no chapterImages found');
     }
-    return false;
 }
 
-function start(i) {
-    if (i < si + sum) {
-        let sub = Math.floor(Math.log10(i));
-        let url = url1.substring(0, url1.length - sub) + i + url2;
-        request({
-            url,
-            proxy,
-            headers: {
-                "accept": "image/avif,image/webp,image/apng,image/svg+xml,image/*,*/*;q=0.8",
-                "accept-encoding": "gzip, deflate, br",
-                "accept-language": "en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7",
-                "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36",
-                "referer": referer,
-                "cookie": cookie,
+function downloadImages(images, index) {
+    if (index >= images.length) {
+        console.log(index + ' images downloaded successfully');
+        process.exit(0);
+    }
+    const url = images[index];
+    const filename = `${FROM_FILE_INDEX + index}.jpg`;
+    const filepath = `img/${filename}`;
+    const headers = {
+        "accept": "image/avif,image/webp,image/apng,image/svg+xml,image/*,*/*;q=0.8",
+        "accept-encoding": "gzip, deflate, br",
+        "accept-language": "en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7",
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36",
+        "referer": referer,
+        "cookie": cookie,
+    };
+    const options = {
+        url,
+        proxy,
+        headers,
+    };
+    request(options)
+        .on("response", (res) => {
+            if (res.statusCode != 200) {
+                console.error(`Failed to download ${filename}`);
+                process.exit(1);
+            } else {
+                console.log(`done ${filename}`);
+                downloadImages(images, index + 1);
             }
         })
-            .on("response", (res) => {
-                if (res.statusCode != 200) {
-                    throw new Error(res.statusCode);
-                } else {
-                    console.log("done " + (i));
-                    start(i + 1);
-                }
-            })
-            .on("error", (err) => {
-                console.error(url);
-                console.error(err);
-            })
-            .pipe(fs.createWriteStream('img/' + i + '.jpg'));
-        console.log('img/' + i + '.jpg | ' + url);
-    }
+        .on("error", (err) => {
+            console.error(`Failed to download ${filename}`);
+            console.error(url);
+            console.error(err);
+            process.exit(1);
+        })
+        .pipe(fs.createWriteStream(filepath));
+    console.log(`${filepath} | ${url}`);
 }
 
-if (readHAR()) {
-    start(si);
-} else {
-    console.error('no header found');
-}
+readHTML();
